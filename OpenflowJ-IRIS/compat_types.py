700ce44d9022097fc2c2e8b607264c29c1770eab
Byungjoon Lee <byungjoon.lee@gmail.com>
2013-12-30 23:01:28 +0900

    compat_types.py modified to support struct interface def."

--- OpenflowJ-IRIS/compat_types.py
+++ OpenflowJ-IRIS/compat_types.py
@@ -2,6 +2,7 @@ import types
 import re
 import itertools
 import pprint
+import spec
 from template import *
 
 class Interface:
@@ -54,19 +55,42 @@ class InterfaceForStruct(Interface):
     self.struct = struct
     self.name = struct.name
     # set of all declared names
-    self.decl_names = set()
-    self.decl_refs = {}
-
-    for i in self.struct['body']:
-      if not i.get('name', None): 
-        continue                        # just skip padding declarations
-      self.decl_names.add(i['name'])
-      list = self.decl_refs.get(i['name'], None)
-      if not list:
-        list = []
-        self.decl_refs[ i['name'] ] = list
-      list.append( i )
+#     self.declarations = set()
+    self.declarations = []
+    self.imports = set()
+    self.add_declarations( self.get_declarations(struct) )
+        
+  def add_declarations(self, decls):
+    seen = set( self.declarations )
+    for i in decls:
+      if i not in seen:
+        self.declarations.append(i)
+        seen.add(i)
+          
+  def get_declarations(self, struct):
     
+    ret = []
+    
+    for i in struct['body']:
+      if not i.get('name', None): continue
+      
+      if i['type'] == 'List': 
+        return_type = 'List<%s>' % i['inner']
+        self.imports.add('import java.util.List;')
+      else:
+        return_type = i['type']
+        prefixed_type = self.name + spec.Spec.convert_to_camel( i['name'] )
+        if struct.spec.get_type(prefixed_type):
+          return_type = prefixed_type
+         
+      get_signature = 'public %s get%s();' % (return_type, spec.Spec.convert_to_camel( i['name'] ))
+      set_signature = 'public %s set%s(%s value);' % (struct.name, spec.Spec.convert_to_camel( i['name'] ), return_type) 
+        
+      ret.append(get_signature)
+      ret.append(set_signature)
+    
+    return ret
+      
   def __repr__(self):
     return '<InterfaceForStruct name:%s>' % (self.name)
   
@@ -77,12 +101,18 @@ class InterfaceForStruct(Interface):
       return None
     
   def merge(self, struct):
-    '''
-    TBD
-    '''
-    return
+    self.add_declarations( self.get_declarations(struct) )
   
   def convert(self):
-    return (None, None)
+    template = Template.get_template('./tpl/interface_for_structs.tpl')
+    if self.struct.supertype:
+      inherit = 'extends %s' % self.struct.supertype
+    else:
+      inherit = ''
+    result = template.safe_substitute({'typename':self.name, 
+                                       'imports':'\n'.join(self.imports),
+                                       'inherit':inherit,
+                                       'accessors':'\n\n\t'.join(self.declarations)})
+    return (self.name, result)
   
   
