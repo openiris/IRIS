44a4793ec5ce66668b086ef871c13e20b97b43bc
Byungjoon Lee <byungjoon.lee@gmail.com>
2014-02-06 12:36:29 +0900

    Torpedo.jar updated.

--- OpenflowJ-IRIS/src/org/openflow/protocol/OFPort.java
+++ OpenflowJ-IRIS/src/org/openflow/protocol/OFPort.java
@@ -56,6 +56,9 @@ public class OFPort {
 		if ( o == null || !(o instanceof OFPort) ) {
 			return false;
 		}
+		if ( this == o ) {
+			return true;
+		}
 		return this.port == ((OFPort)o).port;
 	}
 	
