5fcc33c4c01529085bf995d86708bc87aee1ed11
uni2u <zuno48@gmail.com>
2014-10-27 08:20:09 +0900

    binding_vif_details = false

--- Torpedo/src/etri/sdn/controller/module/ml2/NetworkConfiguration.java
+++ Torpedo/src/etri/sdn/controller/module/ml2/NetworkConfiguration.java
@@ -8,21 +8,27 @@ class NetworkConfiguration extends OFModel {
 
 	private OFMOpenstackML2Connector parent = null;
 	private RESTApi[] apis = null;
-	
+
 	public NetworkConfiguration(OFMOpenstackML2Connector parent) 
 	{
 		this.parent = parent;
 		this.apis = Arrays.asList(
-			new RESTApi(RestCreateNetwork.URI, new RestCreateNetwork(this)),
-			new RESTApi(RestCreatePort.URI, new RestCreatePort(this)),
-			new RESTApi(RestCreateSubnet.URI, new RestCreateSubnet(this))
+			new RESTApi(RestNetwork.neutronNetAll, new RestNetwork(this)),
+			new RESTApi(RestNetwork.neutronNet, new RestNetwork(this)),
+			new RESTApi(RestNetwork.neutronNetIRIS, new RestNetwork(this)),
+			new RESTApi(RestPort.neutronPortAll, new RestPort(this)),
+			new RESTApi(RestPort.neutronPort, new RestPort(this)),
+			new RESTApi(RestPort.neutronPortIRIS, new RestPort(this)),
+			new RESTApi(RestSubnet.neutronSubnetAll, new RestSubnet(this)),
+			new RESTApi(RestSubnet.neutronSubnet, new RestSubnet(this)),
+			new RESTApi(RestSubnet.neutronSubnetIRIS, new RestSubnet(this))
 		).toArray( new RESTApi[0] );
 	}
-	
+
 	OFMOpenstackML2Connector getModule() {
 		return this.parent;
 	}
-	
+
 	@Override
 	public RESTApi[] getAllRestApi() {
 		return this.apis;
