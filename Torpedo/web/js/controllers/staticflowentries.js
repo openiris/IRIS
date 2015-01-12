1edb3a94516eb547d1c84c1ce190a0048a95eebe
kjwon15 <kjwonmail@gmail.com>
2015-01-12 16:09:52 +0900

    Add, remove staticflowentries

--- Torpedo/web/js/controllers/staticflowentries.js
+++ Torpedo/web/js/controllers/staticflowentries.js
@@ -1,20 +1,49 @@
 irisApp.controller(
-  'CntlStaticFlowEntries',
+  'CntlStaticFlowEntryManager',
   ['$scope', '$http', '$timeout',
     function($scope, $http, $timeout) {
-      $scope.staticflowentries = [];
+      $scope.switch_dpids = [];
+      $scope.rules = [];
+
+      $scope.addEntry = function() {
+        var form = $scope.form;
+        form.active = true;
+        $http.post('/wm/staticflowentry/json', form);
+        // TODO
+      };
+
+      $scope.deleteEntry = function() {
+        var name = $scope.form.name;
+        $http.delete('/wm/staticflowentry/delete/' + name + '/json');
+        // TODO
+      };
+
       $scope.getData = function() {
+        // Get static flow entries.
         $http.get('/wm/staticflowentry/list/all/json')
         .success(function(data){
-          _.each(data, function(entry){
-            $scope.staticflowentries = [];
-            $scope.staticflowentries.push(entry);
+          $scope.rules = [];
+          _.each(data, function(entry) {
+            $scope.rules.push(entry);
           });
         })
         .error(function(){
-          $scope.staticflowentries = [];
+          $scope.rules = [];
         });
 
+        // Get switches.
+        $http.get('/wm/core/controller/switches/json')
+        .success(function(data) {
+          $scope.switch_dpids = [];
+          _.each(data, function(sw) {
+            $scope.switch_dpids.push(sw.dpid);
+          });
+        })
+        .error(function(){
+          $scope.switch_dpids = [];
+        });
+
+        // Loop.
         if ($scope.goon != false) {
           $timeout($scope.getData, 1000);
         }
